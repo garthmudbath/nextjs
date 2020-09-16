5dafcee8a21333d0a43f8997663ccaeaaaf88f52
Garth Stevens <garth@mudbath.com.au>
2020-09-16 15:07:13 +1000

    wip

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,20 @@
-import Head from "next/head";
 import { useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  const [count, setCount] = useState({});
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
-    console.log("REACT Mounted");
+    setCount(4);
   }, []);
 
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-      </Head>
-
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-        <pre>{JSON.stringify(count)}</pre>
+        <pre>{count}</pre>
 
         <p className={styles.description}>
           Get started by editing{" "}
